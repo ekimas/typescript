9eb507ecfdadd2c3c40d9fd29cebfd8a04a9a090
Michal Traskowski <Michal.Traskowski@sunzinet.com>
2023-06-10 08:54:55 +0200

    [FEATURE] Adjust user status endpoint
    Now it return all users

--- src/backend/routes/status/get.user.status.ts
+++ src/backend/routes/status/get.user.status.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from 'express'
 import { TRoute } from '../types'
+import { prisma } from '../../database'
 export default {
     method: 'get',
-    path: '/api/status',
+    path: '/api/user/status',
     validators: [],
     handler: async (req: Request, res: Response) => {
-        res.send(`I'm alive!`)
+        const allUsers = await prisma.user.findMany()
+        res.send(allUsers)
     },
 } as TRoute
